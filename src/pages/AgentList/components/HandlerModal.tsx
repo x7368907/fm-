4ce3d81936ded6723892fe8bf59c849a83d4c518
Ren Lee <x7368907@gmail.com>
2025-11-27 09:27:41 +0800

    修正錯誤

--- src/pages/AgentList/components/HandlerModal.tsx
+++ src/pages/AgentList/components/HandlerModal.tsx
@@ -1,4 +1,3 @@
-import React from 'react'
 import { Modal, Table, Tag } from 'antd'
 import type { ColumnsType } from 'antd/es/table'
 
--- src/pages/AgentList/components/PointCreat.tsx
+++ src/pages/AgentList/components/PointCreat.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react'
+import { useEffect, useState } from 'react'
 import { Form, Input, Select, Button, Checkbox, Tag, InputNumber } from 'antd'
 import { SearchOutlined } from '@ant-design/icons'
 import type { CheckboxChangeEvent } from 'antd/es/checkbox'
